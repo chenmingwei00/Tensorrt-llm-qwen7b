9caccfc29637a26256c5e1d2be2d33e330f22ed9
chenmingwei53 <7864721+chenmingwei53@user.noreply.gitee.com>
2023-09-14 11:39:59 +0800

    prepare.sh 上传

---
+++ tensorrt_llm_july-release-v1/qwenb_chen/install_package.sh
@@ -0,0 +1,4 @@
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+apt-get install git-lfs
+pip install tiktoken -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install onnx_graphsurgeon -i https://pypi.tuna.tsinghua.edu.cn/simple
