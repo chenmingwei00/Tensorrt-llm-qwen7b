90dc13c4383285b56c5f4090e20c73a5d20c19e5
chenmingwei53 <7864721+chenmingwei53@user.noreply.gitee.com>
2023-09-14 16:10:57 +0800

    summary数据文件上传

--- tensorrt_llm_july-release-v1/qwenb_chen/install_package.sh
+++ tensorrt_llm_july-release-v1/qwenb_chen/install_package.sh
@@ -3,3 +3,5 @@ apt-get install git-lfs
 pip install tiktoken==0.4.0 -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install onnx_graphsurgeon==0.3.27 -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install datasets==2.3.2 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install nltk==3.8.1 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install rouge-score==0.1.2 -i https://pypi.tuna.tsinghua.edu.cn/simple
