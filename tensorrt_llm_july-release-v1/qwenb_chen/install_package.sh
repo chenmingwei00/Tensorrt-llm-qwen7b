abb3a7c893b7abb8be0bf419c6c82710b0f3a31e
chenmingwei53 <7864721+chenmingwei53@user.noreply.gitee.com>
2023-09-14 14:25:01 +0800

    summary数据文件上传

--- tensorrt_llm_july-release-v1/qwenb_chen/install_package.sh
+++ tensorrt_llm_july-release-v1/qwenb_chen/install_package.sh
@@ -1,4 +1,5 @@
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
 apt-get install git-lfs
-pip install tiktoken -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install onnx_graphsurgeon -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install tiktoken==0.4.0 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install onnx_graphsurgeon==0.3.27 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install datasets==2.3.2 -i https://pypi.tuna.tsinghua.edu.cn/simple
